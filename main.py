2d946eb7c0721c8ac7ff4346110bc019c826591b
mistisanto7 <mistisanto7@gmail.com>
2026-09-17 15:56:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def root():
     return {"message": "Hello World!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8787))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
